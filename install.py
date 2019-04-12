b5940f9c2dbbc8d5efa3b2be0d81e19ca3171e2b
Timo Häkli <timo.hakli@kuvaverkko.fi>
2019-04-12 18:22:46 +0300

    improved installer

--- install.py
+++ install.py
@@ -1,12 +1,32 @@
 import sys
+from settings import Settings
 
-print("Running installer...")
+class Module():
+    def __init__(self, *args, **kwargs):
+        self.parent = kwargs["parent"]
+        self.settings = kwargs["settings"]
 
-try:
-    import uasyncio
-except ImportError:
-    print("asyncio package not installed, installing")
-    import upip
-    upip.install(['micropython-uasyncio'])
 
-print("Installer ready")
+class Installer():
+    def __init__(self, *args, **kwargs):
+        print("Starting installer...")
+        settings = Settings()
+
+        from wifi import Wifi
+        w=Wifi(parent=self,settings=settings.modules[0]["settings"])
+        w.connect()
+
+    def wifi_on_connect_callback(self):
+        try:
+            import uasyncio
+        except ImportError:
+            print("asyncio package not installed, installing")
+            import upip
+            upip.install(['micropython-uasyncio'])
+
+    def call_callbacks(self, callback_name, **kwargs):
+        if hasattr(self,callback_name):
+            cb = getattr(self,callback_name)
+            cb(**kwargs)
+
+i=Installer()
--- put_files.sh
+++ put_files.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-ampy -p /dev/tty.SLAB_USBtoUART run install.py
 ampy -p /dev/tty.SLAB_USBtoUART put settings-example.json settings.json
+ampy -p /dev/tty.SLAB_USBtoUART put settings.py
+ampy -p /dev/tty.SLAB_USBtoUART put wifi.py
+ampy -p /dev/tty.SLAB_USBtoUART run install.py
+ampy -p /dev/tty.SLAB_USBtoUART mkdir lib
 ampy -p /dev/tty.SLAB_USBtoUART put lib/aswitch.py lib/aswitch.py
 ampy -p /dev/tty.SLAB_USBtoUART put lib/ssd1306.py lib/ssd1306.py
+ampy -p /dev/tty.SLAB_USBtoUART put lib/rotary_irq_esp.py lib/rotary_irq_esp.py
 ampy -p /dev/tty.SLAB_USBtoUART put boot.py
 ampy -p /dev/tty.SLAB_USBtoUART put button_period.py
 ampy -p /dev/tty.SLAB_USBtoUART put button.py
@@ -14,7 +18,5 @@ ampy -p /dev/tty.SLAB_USBtoUART put oled.py
 ampy -p /dev/tty.SLAB_USBtoUART put period.py
 ampy -p /dev/tty.SLAB_USBtoUART put relay.py
 ampy -p /dev/tty.SLAB_USBtoUART put servo.py
-ampy -p /dev/tty.SLAB_USBtoUART put settings.py
 ampy -p /dev/tty.SLAB_USBtoUART put switch.py
-ampy -p /dev/tty.SLAB_USBtoUART put wifi.py
 ampy -p /dev/tty.SLAB_USBtoUART put main.py
